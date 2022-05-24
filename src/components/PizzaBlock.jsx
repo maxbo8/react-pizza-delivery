1dcc43adf5f2f868fffe6e1cb3133d3e0839c1c4
Max Bobylev <rzibms@gmail.com>
2022-05-24 18:30:13 +0700

    fix style, add icon components

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -4,7 +4,7 @@ function PizzaBlock(props) {
         <div className="pizza-block">
             <img
                 className="pizza-block__image"
-                src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+                src="https://pngimg.com/uploads/pizza/pizza_PNG44092.png"
                 alt="Pizza"
             />
             <h4 className="pizza-block__title">{props.title}</h4>
